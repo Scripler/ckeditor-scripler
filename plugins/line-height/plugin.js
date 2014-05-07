ed2745265c0c838e113e6555f0f73d6a8c4e3d12
Gabriel <gabriel@scripler.com>
2014-05-07 14:41:40 +0200

    fixed line height plugin

--- plugins/line-height/plugin.js
+++ plugins/line-height/plugin.js
@@ -4,43 +4,55 @@ CKEDITOR.plugins.add( 'line-height', {
         var config = editor.config,
             lang = editor.lang.format;
         var trackings = [];
-        
+
         for (var i = 1; i < 5; i=i+0.1) {
           i = Math.round(i * 100) / 100;
           trackings.push(String(i));
         }
-        
+
         editor.ui.addRichCombo('line-height', {
           label: 'Line-Height',
           //title: 'Change line-height',
           voiceLabel: 'Change line-height',
           className: 'cke_format',
           multiSelect: false,
-          
+
           panel: {
             css : [ config.contentsCss, CKEDITOR.getUrl( CKEDITOR.skin.getPath + 'editor.css' ) ],
             voiceLabel : lang.panelVoiceLabel
           },
-          
+
           init: function() {
             //this.startGroup('line-height');
             for (var this_letting in trackings) {
               this.add(trackings[this_letting], trackings[this_letting], trackings[this_letting]);
             }
           },
-          
+
           onClick: function(value) {
             editor.focus();
             editor.fire('saveSnapshot');
-            var ep = editor.elementPath();
 
-            var style = new CKEDITOR.style({styles: {'line-height': value}});
-        
-	    for (var i = 0; i < ep.elements.length; i++) {
-		    if ( ep.elements[i].getName() === "p" ) {
-			style.applyToObject( ep.elements[i], editor )
-		    }    
-            }
+			var selection = editor.getSelection(),
+			range = selection && selection.getRanges()[ 0 ],
+			nearestListBlock;
+
+			if ( !selection ) {
+				return;
+			}
+
+			var iterator = range.createIterator(),
+			enterMode = editor.config.enterMode,
+			block;
+
+			iterator.enforceRealBlocks = true;
+			iterator.enlargeBr = enterMode != CKEDITOR.ENTER_BR;
+
+			while ( ( block = iterator.getNextParagraph( enterMode == CKEDITOR.ENTER_P ? 'p' : 'div' ) ) ) {
+				if ( !block.isReadOnly() ) {
+					block.$.style.lineHeight = value;
+				}
+			}
 
             editor.fire('saveSnapshot');
           }
