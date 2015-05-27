48627da4ccec0ae89b857263133d60f1f5a5325b
Allan Mørk Christensen <allanmc@gmail.com>
2015-05-27 20:25:13 +0200

    Deleted old out-commented code.

--- plugins/scripler/plugin.js
+++ plugins/scripler/plugin.js
@@ -1,209 +1,65 @@
 CKEDITOR.plugins.add( 'scripler', {
-    init: function( editor ) {
-        function isEmpty( node ) {
-            var trimmed;
+	init: function( editor ) {
+		function isEmpty( node ) {
+			var trimmed;
 			if (node.getData) {
 				trimmed = node.getData();
 			} else {
 				trimmed = node.getText();
 			}
 			trimmed = CKEDITOR.tools.ltrim(trimmed);
-            if (trimmed && trimmed.length > 0) {
-                //Paragraph contains some text, so not empty
-                return false;
-            }
-
-            //Didn't identify any content, so should be empty
-            return true;
-        }
-
-        function checkBlock(block) {
-            if (block && block.is('p')) {
-                if (isEmpty(block)) {
-                    block.addClass('empty-paragraph');
-                } else {
-                    block.removeClass('empty-paragraph');
-                }
-            }
-        }
-
-        var changed = function () {
-            if (editor.elementPath() && editor.elementPath().block) {
-                // Check the block where the users cursor is currently located.
-                checkBlock(editor.elementPath().block);
-                // Check the previous block (maybe we just left it empty, while auto paragraphing to the next block)
-                checkBlock(editor.elementPath().block.getPrevious());
-            }
-
-            //Reset change timer
-            //resetChangeTimeout();
-        };
-
-        // For any content check, check for empty paragraph.
-        editor.on('change', changed);
-        // Make sure that we identify an empty paragraph after auto-paragraphing.
-        editor.on('selectionChange', changed);
-        editor.on('elementsPathUpdate', changed);
-
-
-        //editor.on('key', resetChangeTimeout);
-
-        var toolbarObj;
-		var editorObj;
-		var inFocus = false;
-		var fading = true;
-		var op = 0.0;
-		var timerFadeIn;
-		var timerFadeOut;
-		var timerChangeTimeout;
-		var instanceName = 'bodyeditor';
-
-		function showToolbar() {
-            fadeIn(toolbarObj);
-		}
-
-		function hideToolbar(force) {
-			//console.log('hide toolbar');
-			//Hide Toolbar
-			if (force) {
-				fading = true;
-				if (doAbortToolbarHide()) {
-					fading = false;
-					return;
-				}
-				clearInterval(timerFadeOut);
-				clearInterval(timerFadeIn);
-				clearTimeout(timerChangeTimeout);
-				toolbarObj.style.display = 'none';
-				toolbarObj.style.opacity = 0.0;
-				toolbarObj.style.filter = 'alpha(opacity=0)';
-			} else if (!inFocus) {
-				fadeOut(toolbarObj);
+			if (trimmed && trimmed.length > 0) {
+				//Paragraph contains some text, so not empty
+				return false;
 			}
+
+			//Didn't identify any content, so should be empty
+			return true;
 		}
 
-		function resetChangeTimeout() {
-			//console.log('wow-'+Math.floor(Math.random()*101));
-			if (timerChangeTimeout) {clearTimeout(timerChangeTimeout)};
-			timerChangeTimeout = setTimeout(function(){fadeOut(toolbarObj)}, 5000);//5 seconds
-		}
-
-		function fadeOut(element) {
-			if (!fading) {
-				fading = true;
-				var delay = 0;
-				clearInterval(timerFadeIn);
-
-				//Done hide if any open panels
-				if (doAbortToolbarHide()) {
-					fading = false;
-					return;
-				}
-
-				//Fade toolbar
-				timerFadeOut = setInterval(function () {
-					if (!delay) {
-						if (op <= 0.1){
-							clearInterval(timerFadeOut);
-							element.style.display = 'none';
-							op = 0.0;
-						}
-						element.style.opacity = op;
-						element.style.filter = 'alpha(opacity=' + op * 100 + ')';
-						op -= 0.1;
-					} else {
-						delay--;
-					}
-				}, 25);
-			}
-		}
-
-		function doAbortToolbarHide() {
-			var panels = document.querySelectorAll('div.cke_panel');
-			for (var i = 0;i<panels.length;i++){
-				if (panels[i].style.display!='none') {
-					//console.log(panels[i]);
-					//panels[i].style.display = 'none';
-					//console.log('Cancelled hide')
-					return true;
+		function checkBlock(block) {
+			if (block && block.is('p')) {
+				if (isEmpty(block)) {
+					block.addClass('empty-paragraph');
+				} else {
+					block.removeClass('empty-paragraph');
 				}
 			}
-			return false;
 		}
 
-		function fadeIn(element) {
-			if (fading) {
-				fading = false;
-				op = 1;
-				clearInterval(timerFadeOut);
-				element.style.opacity = op;
-				element.style.filter = 'alpha(opacity=' + op * 100 + ')';
-				element.style.display = 'block';
+		var changed = function () {
+			if (editor.elementPath() && editor.elementPath().block) {
+				// Check the block where the users cursor is currently located.
+				checkBlock(editor.elementPath().block);
+				// Check the previous block (maybe we just left it empty, while auto paragraphing to the next block)
+				checkBlock(editor.elementPath().block.getPrevious());
 			}
-			//resetChangeTimeout();
-		}
+		};
 
-        editor.on('paste', function (ev) {
-            //onPaste();
+		// For any content check, check for empty paragraph.
+		editor.on('change', changed);
+		// Make sure that we identify an empty paragraph after auto-paragraphing.
+		editor.on('selectionChange', changed);
+		editor.on('elementsPathUpdate', changed);
 
+		var toolbarObj;
+
+		editor.on('paste', function (ev) {
 			//Identify empty paragraphs in pasted data
-			//alert("Pasted 1");
 			if (ev.data.dataValue) {
 				ev.data.dataValue = ev.data.dataValue.replace(/(<p)(?![^>]*empty-paragraph)([^>]*?)(class\s*=\s*["']([^"']*)["']([^>]*))?(>(\s|&nbsp;)*<\/p>)/g, '$1$2 class="$4 empty-paragraph"$5$6');
 			}
-			//alert("Pasted 2");
-            //var innerDocument = editor.$.document;
-
-            /*var checkPasteDone = setInterval(function(){
-                //alert('Readystate: ' + CKEDITOR.instances.editor1.window.$.document.readyState);
-				var readyState = CKEDITOR.instances[instanceName].window.$.document.readyState;
-				//console.log(readyState);
-                if (!/in/.test(readyState) || readyState=="interactive") {
-					clearInterval(checkPasteDone);
-				}
-            },100);*/
-            //setInterval(function(){alert('Readystate: ' + CKEDITOR.instances.editor1.window.$.document.readyState);},5000);
 		});
 
-		var maskElm = document.getElementById('mask');
-		//var onPaste = function () {
-		//	maskElm.style.display = 'block';
-			//console.log('Pasting...');
-		//}
-
-        // Hide/show toolbar
-        //editor.on('focus', function () {inFocus = true; showToolbar();});
-        //editor.on('blur', function () {inFocus = false; hideToolbar(true);});
-        editor.on('instanceReady', function (event){
-			//console.log(JSON.stringify(CKEDITOR.instances.editor1.window.$.document.getElementsByTagName("body")[0]));
-
-			var editableBody = CKEDITOR.instances[instanceName].window.$.document.getElementsByTagName("body")[0]
-			//editableBody.addEventListener ("paste", onPaste, false);
-			//editableBody.addEventListener ("beforepaste", onPaste, false);
-
-			//var editorDocument = CKEDITOR.instances[instanceName].window.$.document;
+		editor.on('instanceReady', function (event){
 			var editorId = editor.id;
 			var editorTop = document.getElementById( editorId+'_top' );
 			toolbarObj = document.getElementById( editorId+'_toolbox' );
-			editorObj = document.getElementById( editorId+'_contents' );
-			//toolbarArea = document.getElementById( 'toolbar-area' );
 			editorTop.style.textAlign = 'center';
 
 			toolbarObj.style.display = 'inline-block';
 			toolbarObj.style.background = '#D6D6D6';
 			toolbarObj.style.padding = '3px 0 0 7px';
-
-			//toolbarObj.onmousemove = showToolbar;
-			//toolbarArea.onmousemove = showToolbar;
-			//editorObj.onmouseover = showToolbar;
-			//toolbarObj.onmouseout = function () {hideToolbar(false) };
-			//toolbarArea.onmouseout = function () {hideToolbar(false) };
-			//editorObj.onmouseout = function () {hideToolbar(false) };
-			//editableBody.onblur = function () {inFocus = false; hideToolbar(true);};
-			//editableBody.onfocus = function () {inFocus = true; showToolbar();};
-			//toolbarObj.onmouseover = showToolbar;
-			//editorDocument.onmousemove = showToolbar;
-			//showToolbar();
-        });
-    }
+		});
+	}
 });
